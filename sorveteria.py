3fba7deea49361679d0666016748a15871016d00
Alfredo Neto <alfredoneto934@gmail.com>
2023-08-26 03:44:00 -0300

    refactor: redoing with more lines and identation

--- sorveteria.py
+++ sorveteria.py
@@ -1,29 +1,46 @@
 class Sorveteria:
 
     def tabulate(self, data, headers):
-        # Encontrar a largura ideal para cada coluna
-        column_widths = [max(len(str(item)) for item in col) for col in zip(headers, *data)]
+    # Encontrar a largura ideal para cada coluna
+        column_widths = [
+            max(len(str(item)) 
+                for item in col) 
+                  for col in zip(headers, *data)
+        ]
 
         # Imprimir a linha superior
-        print("+" + "+".join("-" * (width + 2) for width in column_widths) + "+")
+        line = "+"
+        for width in column_widths:
+            line += "-" * (width + 2) + "+"
+        print(line)
 
         # Imprimir cabeçalhos
-        print("| " + " | ".join(word.ljust(width) for word, width in zip(headers, column_widths)) + " |")
-        print("+" + "+".join("-" * (width + 2) for width in column_widths) + "+")
+        header_line = "| "
+        for word, width in zip(headers, column_widths):
+            header_line += word.ljust(width) + " | "
+        print(header_line)
+        print(line)
 
         # Imprimir os dados da tabela
         for row in data:
-            print("| " + " | ".join(word.ljust(width) for word, width in zip(row, column_widths)) + " |")
-            print("+" + "+".join("-" * (width + 2) for width in column_widths) + "+")
-
-
+            data_line = "| "
+            for word, width in zip(row, column_widths):
+                data_line += word.ljust(width) + " | "
+            print(data_line)
+            print(line)
 
 data = [
-    ["row1 column1", "row1 column2"],
-    ["row2 column1", "row2 column2"]
+    ["1", "R$ 6,00", "R$ 7,00", "R$ 8,00"],
+    ["2", "R$ 10,00", "R$ 12,00", "R$ 14,00"],
+    ["3", "R$ 14,00","R$ 17,00", "R$ 20,00"]
 ]
 
-headers = ["Heading1", "Heading2"]
+headers = [
+           "No DE BOLAS", 
+           "Sabor Tradicional (tr)", 
+           "Sabor Premium (pr)", 
+           "Sabor Especial (es)"
+          ]
 
 sorveteria = Sorveteria()
 sorveteria.tabulate(data, headers)

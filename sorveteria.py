43e81f1a2089a2a77a6b15acc8975c1e5cddc12d
Alfredo Neto <alfredoneto934@gmail.com>
2023-08-27 23:57:17 -0300

    added pegar_pedidos

--- sorveteria.py
+++ sorveteria.py
@@ -2,24 +2,103 @@ class Sorveteria:
 		
 		def __init__(self):
 			self._boas_vindas = None
-			self._data = [
+			self._flavour = None
+			self._num_of_icecream_scoops = None
+			self._table_data = [
 				["1", "R$ 6,00", "R$ 7,00", "R$ 8,00"],
 				["2", "R$ 10,00", "R$ 12,00", "R$ 14,00"],
 				["3", "R$ 14,00","R$ 17,00", "R$ 20,00"]
 			]
 
-			self._headers = [
+			self._table_headers = [
 				"Nº DE BOLAS", 
 				"Sabor Tradicional (tr)", 
 				"Sabor Premium (pr)", 
 				"Sabor Especial (es)"
 			]
 
+		# encapsulando valores privados com getters e setters
+		@property
+		def flavour(self):
+			return self._flavour
+		
+		# encapsulando valores privados com getters e setters
+		@property
+		def num_of_icecream_scoops(self):
+			return self._num_of_icecream_scoops
+
+		# encapsulando valores privados com getters e setters
+		@flavour.setter
+		def flavour(self, novo_valor):
+			self._flavour = novo_valor
+		
+		# encapsulando valores privados com getters e setters
+		@num_of_icecream_scoops.setter
+		def num_of_icecream_scoops(self, novo_valor):
+			self._num_of_icecream_scoops = novo_valor
+
     # método que atribui um valor para um atributo do objeto e printa uma mensagem na tela
 		def boas_vindas(self):
 			self._boas_vindas = "Bem-vindo a Sorveteria do Alfredo de Aguiar Braule Pinto Neto"
 			print(self._boas_vindas)
 			self._tabulate(self._data, self._headers)
+
+		def pegar_pedidos(self, user_flavour, user_scoop):
+			scoops = [
+				["1", "R$ 6,00", "R$ 7,00", "R$ 8,00"],
+				["2", "R$ 10,00", "R$ 12,00", "R$ 14,00"],
+				["3", "R$ 14,00","R$ 17,00", "R$ 20,00"]
+			]
+
+			flavours = {
+				"tr": "TRADICIONAL", 
+				"pr": "PREMIUM", 
+				"es": "ESPECIAL"
+			}
+
+			preco, sabor, bola = '', '', ''
+			for index, (flavour, value) in enumerate(flavours.items()):
+				if flavour == user_flavour:
+					sabor = value
+					for scoop in scoops:
+						if scoop[0] == str(user_scoop):
+							bola = scoop[0]
+							preco = scoop[index + 1]
+			
+			plural = 'bola'
+			if int(bola) > 1:
+				plural += "s"
+			formatted_string = "Você pediu {} {} de sorvete no sabor {}: {}".format(bola, plural, sabor, preco)	
+			print(formatted_string)
+			return True
+
+
+		def pegar_sabor_do_sorvete(self):
+			self.flavour = input("Entre com o Sabor desejado (tr/es/pr): ")
+			if self._is_valid_flavour(self.flavour):
+				return self.flavour
+			return False
+		
+		def pegar_num_de_scoops(self):
+			self.num_of_icecream_scoops = input("Entre com o número de bolas de sorvete desejado (1/2/3): ")
+			print(self.num_of_icecream_scoops)
+			if self._is_valid_number_of_icecream_scoops(self.num_of_icecream_scoops):
+				return self.num_of_icecream_scoops
+			return False
+		
+		def _is_valid_flavour(self, value):
+			if value not in ["tr", "es", "pr"]:
+				print("Sabor inválido. Tente novamente")
+				return False
+			
+			return True
+		
+		def _is_valid_number_of_icecream_scoops(self, value):
+			if value not in ["1", "2", "3"]:
+				print("Número de bolas de sorvete inválido. Tente novamente.")
+				return False
+			
+			return True
 
 		def _tabulate(self, data, headers):
 			# Encontrar a largura ideal para cada coluna
@@ -55,4 +134,7 @@ class Sorveteria:
 					print(line)
 
 sorveteria = Sorveteria()
-sorveteria.boas_vindas()
+user_flavour = "es"
+user_scoop = "3"
+result = sorveteria.pegar_pedidos(user_flavour, user_scoop)
+print(result)
--- sorveteria_test.py
+++ sorveteria_test.py
@@ -11,19 +11,34 @@ class SorveteriaTest(UnitTest):
     @mock.patch('sorveteria.input', create=True)
     def test_pegar_valor_do_produto(self, mocked_input):
         with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            mocked_input.side_effect = ["a", "5", "-5"]
+            mocked_input.side_effect = ["aa"]
 
             sorveteria = Sorveteria()
 
-            result = sorveteria.pegar_valor_do_produto()
-            self._assert(mock_stdout.getvalue() == "Insira somente valores numéricos\n")
+            result = sorveteria.pegar_sabor_do_sorvete()
+            self._assert(mock_stdout.getvalue() == "Sabor inválido. Tente novamente\n")
             self._assert(result == False)
 
-            result = sorveteria.pegar_valor_do_produto()
-            self._assert(result == 5.0)
+    @mock.patch('sorveteria.input', create=True)
+    def test_pegar_num_de_scoops(self, mocked_input):
+        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            mocked_input.side_effect = ["a"]
 
-            result = sorveteria.pegar_valor_do_produto()
-            # self._assert(mock_stdout.getvalue() == "Insira valores válidos\n")
+            sorveteria = Sorveteria()
+            
+            result = sorveteria.pegar_num_de_scoops()
+            # self._assert(mock_stdout.getvalue() == "Número de bolas de sorvete inválido. Tente novamente.\n")
             self._assert(result == False)
+    
+    @mock.patch('sorveteria.input', create=True)
+    def test_pegar_pedidos(self, mocked_input):
+        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            sorveteria = Sorveteria()
+            user_flavour = "tr"
+            user_scoop = "1"
+            
+            result = sorveteria.pegar_pedidos(user_flavour, user_scoop)
+            self._assert(mock_stdout.getvalue() == "Você pediu 1 bola de sorvete no sabor TRADICIONAL: R$ 6,00.\n")
+            self._assert(result == True)
 
 SorveteriaTest.call()
